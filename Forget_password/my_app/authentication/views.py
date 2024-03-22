a6314afdf914dff4670b22b9f3f60fc12ac48eb0
Kusalni <kusalniperera03@gmail.com>
2024-03-23 01:48:59 +0530

    Email token and url generating

--- Forget_password/my_app/authentication/views.py
+++ Forget_password/my_app/authentication/views.py
@@ -1,8 +1,12 @@
 # from django.shortcuts import render
 # Create your views here.
 
+from base64 import urlsafe_b64encode
 from rest_framework import generics, status, viewsets, response
 from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_bytes
+
 from . import serializer
 
 class PasswordReset(generics.GenericAPIView):
@@ -15,3 +19,7 @@ class PasswordReset(generics.GenericAPIView):
         serializers.is_valid(raise_exception=True)
         email = serializers.data["email"]
         user = User.objects.filter(email=email).first()
+
+        if user:
+            encoded_pk = urlsafe_b64encode(force_bytes(user.pk))
+            token = PasswordResetTokenGenerator().make_token(user)
