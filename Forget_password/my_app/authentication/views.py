c8f05a6d863e7dfa5942b620545b5216ca2b79e1
Kusalni <kusalniperera03@gmail.com>
2024-03-23 01:37:37 +0530

    Password reset view

--- Forget_password/my_app/authentication/views.py
+++ Forget_password/my_app/authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
 # Create your views here.
+
+from rest_framework import generics, status, viewsets, response
+from django.contrib.auth.models import User
+from . import serializer
+
+class PasswordReset(generics.GenericAPIView):
+
+    serializer_class = serializer.EmailSerializer
+
+    def post (self, request):
+
+        serializers = self.serializer_class(data = request.data)
+        serializers.is_valid(raise_exception=True)
+        email = serializers.data["email"]
+        user = User.objects.filter(email=email).first()
