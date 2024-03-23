28d1261732a1c219c7e7892822c1f31f3a643f07
Kusalni <kusalniperera03@gmail.com>
2024-03-23 10:48:21 +0530

    Validation for user existence

--- Forget_password/my_app/authentication/views.py
+++ Forget_password/my_app/authentication/views.py
@@ -5,6 +5,7 @@ from base64 import urlsafe_b64encode
 from rest_framework import generics, status, viewsets, response
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.urls import reverse
 from django.utils.encoding import force_bytes
 
 from . import serializer
@@ -23,3 +24,25 @@ class PasswordReset(generics.GenericAPIView):
         if user:
             encoded_pk = urlsafe_b64encode(force_bytes(user.pk))
             token = PasswordResetTokenGenerator().make_token(user)
+
+            #localhost:8000/reset-password/<encoded_pk>/<token>
+
+            reset_url = reverse(
+                "reset-password",
+                kwargs = {"encoded_pk":encoded_pk, "token":token}
+            )
+
+            reset_url = f"localhost:8000{reset_url}"
+
+            return response.Response(
+                {
+                    "message":
+                    f"Your password reset link:{reset_url}"
+                },
+                status = status.HTTP_200_OK,
+            )
+        else:
+            return response.Response(
+                {"message":"User doesn't exists"},
+                status = status.HTTP_400_BAD_REQUEST,
+            )
