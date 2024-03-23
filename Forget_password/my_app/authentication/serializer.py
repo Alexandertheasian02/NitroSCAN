ae6cc5b7333997253249ead0bfba5741fb27adf2
Kusalni <kusalniperera03@gmail.com>
2024-03-23 17:46:03 +0530

    Get and validate user information

--- Forget_password/my_app/authentication/serializer.py
+++ Forget_password/my_app/authentication/serializer.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_decode
+
 class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
 
@@ -15,3 +19,22 @@ class ResetPasswordSerializer(serializers.Serializer):
 
     class Meta:
         fields = ("password",)
+
+    def validate(self, data):
+        
+        password = data.get("password")
+        token = self.context.get("kwargs").get("token")
+        encoded_pk = self.context.get("kwargs").get("encoded_pk")
+
+        if token is None or encoded_pk is None:
+            raise serializers.ValidationError("Missing data")
+        
+        pk = urlsafe_base64_decode(encoded_pk).decode()
+        user = User.objects.get(pk = pk)
+
+        if not PasswordResetTokenGenerator().check_token(user, token):
+            raise serializers.ValidationError("The reset token is invalid!")
+        
+        user.set_password(password)
+        user.save()
+        return data 
